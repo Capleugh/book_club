6e83d3fe366236b022acab10446fecb8c26d6402
Carleigh Crockett <capleugh@gmail.com>
2019-12-14 18:44:28 -0700

    Create joins table and relationships

--- spec/model/author_spec.rb
+++ spec/model/author_spec.rb
@@ -4,4 +4,9 @@ RSpec.describe Author, type: :model do
   describe "validations" do
     it {should validate_presence_of :name}
   end
+
+  describe "relationships" do
+    it {should have_many :author_books}
+    it {should have_many(:books).through(:author_books)}
+  end
 end
--- spec/model/book_spec.rb
+++ spec/model/book_spec.rb
@@ -5,5 +5,8 @@ RSpec.describe Book, type: :model do
     it {should validate_presence_of :title}
     it {should validate_presence_of :pages}
     it {should validate_presence_of :publication_year}
+
+    it {should have_many :author_books}
+    it {should have_many(:authors).through(:author_books)}
   end
 end
