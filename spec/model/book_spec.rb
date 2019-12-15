4882f0c853eefdcc45225518867971ecd7c0db20
Carleigh Crockett <capleugh@gmail.com>
2019-12-14 17:50:11 -0700

    Create validations for books and authors

---
+++ spec/model/book_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+RSpec.describe Book, type: :model do
+  describe "validations" do
+    it {should validate_presence_of :title}
+    it {should validate_presence_of :num_pages}
+    it {should validate_presence_of :publication_year}
+  end
+end
